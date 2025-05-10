1fba4fd888cfb073c7f339855529f54c186e2a73
trobsta <troybjorkman@gmail.com>
2025-05-10 13:57:45 +0200

    Add success message + simulator username & password display and update component selector for new member form

--- src/app/new-member/new-member.component.ts
+++ src/app/new-member/new-member.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MemberService } from '../services/member.service';
-import { Member } from '../model/member';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,7 +12,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
-  selector: 'app-add-member',
+  selector: 'app-new-member',
   standalone: true,
   imports: [
     CommonModule,
@@ -27,31 +26,32 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTooltipModule,
   ],
   templateUrl: './new-member.component.html',
-  styleUrl: './new-member.component.css',
+  styleUrls: ['./new-member.component.css'],
 })
 export class NewMemberComponent {
-  member: Member = {
-    memberId: 0,
-    memberType: 'Investor',
+  member = {
+    memberType: '',
     memberEmail: '',
     memberAddress: '',
     memberPhone: '',
   };
+  showSuccessMessage = false;
 
   constructor(private memberService: MemberService, private router: Router) {}
 
   createMember() {
-    // All fields are already validated in the template
-    console.log('Form Submitted:', this.member);
+    // Simulate form submission
+    console.log('Member created:', this.member);
 
-    this.memberService.createMember(this.member).subscribe(
-      (response) => {
-        console.log('Member added successfully:', response);
-        this.router.navigate(['/member']);
-      },
-      (error) => {
-        console.error('Error adding member:', error);
-      }
-    );
+    // Show the success message
+    this.showSuccessMessage = true;
+
+    // Optionally, reset the form
+    this.member = {
+      memberType: '',
+      memberEmail: '',
+      memberAddress: '',
+      memberPhone: '',
+    };
   }
 }
